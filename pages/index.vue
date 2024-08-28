24db7107af8d275b21a373b60895140c83be192b
uyinyan <yinyan2323@qq.com>
2024-08-28 09:44:47 +0800

    a

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
  * @Author: uyinyan yinyan2323@qq.com
  * @Date: 2024-08-27 14:37:59
  * @LastEditors: uyinyan yinyan2323@qq.com
- * @LastEditTime: 2024-08-27 19:15:14
+ * @LastEditTime: 2024-08-27 19:57:58
  * @FilePath: /umi/umiNuxt/pages/index.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
